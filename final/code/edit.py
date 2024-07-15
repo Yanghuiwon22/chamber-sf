9d935df5e385973db19c78465bb9c45d86ac764c
Yanghuiwon22 <hannah4212@naver.com>
2024-07-15 14:48:38 +0900

    toggle_edit 수정 전 저장

--- final/code/edit.py
+++ final/code/edit.py
@@ -7,21 +7,27 @@ class Edit:
         self.font = pygame.font.Font('../font/LycheeSoda.ttf', 30)
         self.display_surface = pygame.display.get_surface()
 
+        self.toggle_edit = toggle_edit
     def input(self):
         mouse = pygame.mouse.get_pressed()
 
+        if not self.timer.active:
+            if mouse[0]:
+                if self.save_rect.collidepoint(pygame.mouse.get_pos()):
+                    print('save')
+
     def display(self):
         save_surf = self.font.render('Save', False, 'Black')
-        save_rect = save_surf.get_rect()
-        save_rect.topleft = SCREEN_WIDTH-10-save_rect.width, SCREEN_HEIGHT-10-save_rect.height
+        self.save_rect = save_surf.get_rect()
+        self.save_rect.topleft = SCREEN_WIDTH-10-self.save_rect.width, SCREEN_HEIGHT-10-self.save_rect.height
 
         editmode_surf = self.font.render('Edit Mode', False, 'Black')
         editmode_rect = editmode_surf.get_rect()
         editmode_rect.topleft = 10, 10
 
-        self.display_surface.blit(save_surf, save_rect)
+        self.display_surface.blit(save_surf, self.save_rect)
         self.display_surface.blit(editmode_surf, editmode_rect)
     def update(self):
-        self.input()
         self.display()
+        self.input()
 
