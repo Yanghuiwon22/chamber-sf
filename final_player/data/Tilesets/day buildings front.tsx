4014f2c54d8cc048a4cd1ec14da9c82847d39d97
202116302 <wls5258@naver.com>
2024-10-08 23:07:33 +0900

    tileset 추가

--- final_player/data/Tilesets/day buildings front.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="day buildings front" tilewidth="16" tileheight="16" tilecount="207" columns="23">
- <image source="../../../../../Users/user/Downloads/city background/day/day buildings front.png" width="380" height="147"/>
-</tileset>
